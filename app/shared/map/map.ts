c900f18e37d94418561e9a870b1bbc5f12b39f56
Bogdan Onikiienko <onikiienko@gmail.com>
2016-01-15 14:04:16 +0200

    map.ts changed

--- app/shared/map/map.ts
+++ app/shared/map/map.ts
@@ -12,16 +12,18 @@ import 'leaflet';
 export class Map {
     name: string = 'map'
     constructor(public location:Location) {
-        buildMap();
+        mapBuilder.build();
     }
 }
 
-function buildMap()  {
-    document.getElementById('mapContainer').innerHTML = '<div id="map" style="width: 600px; height: 400px"></div>';
-    var map = L.map('map').setView([51.505, -0.09], 13);
+let  mapBuilder =  {
+    build() {
+        document.getElementById('mapContainer').innerHTML = '<div id="map" style="width: 600px; height: 400px"></div>';
+        let map = L.map('map').setView([51.505, -0.09], 13);
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
-        maxZoom: 18,
-        id: 'mapbox.streets'
-    }).addTo(map);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
+            maxZoom: 18,
+            id: 'mapbox.streets'
+        }).addTo(map);
+    }
 }
