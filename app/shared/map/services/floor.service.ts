ff88370a14c50341b2956097c8ad8cdfe6fc3cfd
Vitalii Rakovtsii <vitalii.rakovtsii@playtech.com>
2016-01-15 16:35:20 +0200

    Adding remote get call for floor mocks

--- app/shared/map/services/floor.service.ts
+++ app/shared/map/services/floor.service.ts
@@ -1,18 +1,18 @@
-import {Inject, Injectable} from 'angular2/core';
-import {Http} from 'angular2/http';
-import {Floor} from '../models/floor';
+import { Inject, Injectable } from 'angular2/core';
+import { Http } from 'angular2/http';
+import { Floor } from '../models/floor';
 
 @Injectable()
 export class FloorService {
-    private floors: Floor[];
+    private floors:Floor[];
 
-    constructor (
-        @Inject(Http) private http: Http
-    ) {
+    constructor(@Inject(Http) private http:Http) {
         this.floors = [new Floor(20)];
     }
 
-    getFloors(): Promise<Floor[]> {
-        return Promise.resolve(this.floors);
+    getFloors():Promise<Floor[]> {
+        return this.http.get('/public/mocks/floors.json')
+            .map(response => response.json())
+            .toPromise();
     }
 }
